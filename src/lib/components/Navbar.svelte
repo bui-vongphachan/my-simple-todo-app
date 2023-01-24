c02a78e15c3ab4a66bd6bc388a06f61e6a4c50fa
vongphachan~ <vongphachannukkalash@gmail.com>
2023-01-24 09:52:59 +0700

    setup

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,10 @@
+<div class="  bg-red-500">
+	<div class=" container m-auto">
+		<div class="h-[70px] flex justify-between items-center">
+			<h1 class=" text-black prose-xl">Simple Todo App</h1>
+            <div>
+                THIS IS DEMO
+            </div>
+		</div>
+	</div>
+</div>
