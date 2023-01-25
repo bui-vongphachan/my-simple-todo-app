88662b2d77ec97c514f9cee004cce48638fc6af6
vongphachan~ <vongphachannukkalash@gmail.com>
2023-01-25 15:19:00 +0700

    ettempted to log vercel project id

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess({
-		replace: [
-			// this will allow us to use is in the code as import.meta.env.VERCEL_ANALYTICS_ID
-			['import.meta.env.VERCEL_ANALYTICS_ID', JSON.stringify(process.env.VERCEL_ANALYTICS_ID)]
-		]
-	}),
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter()
