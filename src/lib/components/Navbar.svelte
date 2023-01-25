964b1198bde9ef62b3be6951cc48e98ad69e27b5
vongphachan~ <vongphachannukkalash@gmail.com>
2023-01-25 17:40:13 +0700

    removed unused code

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,38 +1,33 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
+	import { goto } from '$app/navigation'; 
 	import { PATH_TO_LOGIN } from '$lib/constants/pagePath';
 	import { isAuthenticated } from '$lib/store/auth';
 	import PageFade from './PageFade.svelte';
 
-	let isSubmiting = false;
-
 	async function logOut() {
-		isSubmiting = true;
 		localStorage.removeItem('user');
 
 		isAuthenticated.set(false);
 
 		goto(PATH_TO_LOGIN);
-
-		isSubmiting = false;
 	}
 </script>
 
-<PageFade>
-	<div class="  bg-green-500">
-		<div class=" container m-auto">
-			<div class="h-[70px] text-white flex justify-between items-center">
-				<a href="/" class=" prose-xl">Simple Todo App</a>
-				<div>
-					<button
-						class="hover:bg-green-600 bg-green-700 px-4 py-2 rounded-md"
-						disabled={isSubmiting}
-						on:click={logOut}
-					>
-						Log out
-					</button>
+{#if !$isAuthenticated}
+	<div />
+{:else}
+	<PageFade>
+		<div class="  bg-green-500">
+			<div class=" container m-auto">
+				<div class="h-[70px] text-white flex justify-between items-center">
+					<a href="/" class=" prose-xl">Simple Todo App</a>
+					<div>
+						<button class="hover:bg-green-600 bg-green-700 px-4 py-2 rounded-md" on:click={logOut}>
+							Log out
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</PageFade>
+	</PageFade>
+{/if}
